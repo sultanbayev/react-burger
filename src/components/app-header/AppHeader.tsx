0582a7fdb1897996e21170dc9473ef6d576fff53
sultanbayev <sultanbayev@gmail.com>
2021-07-18 06:50:36 +0600

    AppHeader component was made

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import styles from './app-header.module.css';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import NavLink from './nav-link/NavLink';
+
 
 function AppHeader() {
     return (
-        <div></div>
+        <header className={styles.header}>
+            <div className={styles.content}>
+                <nav className={styles.nav}>
+                    <ul className={styles.list}>
+                        <li>
+                            <NavLink text="Конструктор" isActive={true}>
+                                <BurgerIcon type="primary" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink text="Лента заказов" isActive={false}>
+                                <ListIcon type="secondary" />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles.logo}>
+                    <Logo />
+                </div>
+                <div>
+                    <NavLink text="Личный кабинет" isActive={false}>
+                        <ProfileIcon type="secondary" />
+                    </NavLink>
+                </div>
+            </div>
+        </header>
     );
     
 }
---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './app.module.css';
+import AppHeader from '../app-header/AppHeader';
+
+function App() {
+  return (
+    <AppHeader></AppHeader>
+  );
+}
+
+export default App;
