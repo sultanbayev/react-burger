82bc7f3ddb391835ff704cb22a79a11f778a2f51
sultanbayev <sultanbayev@gmail.com>
2021-08-08 02:08:25 +0600

    add components actions

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -1,2 +1,5 @@
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const SET_BUN = 'SET_BUN';
+export const ADD_STAFFING = 'ADD_STAFFING';
+export const REMOVE_STAFFING = 'REMOVE_STAFFING';
