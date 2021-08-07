070d27acab033910b6522625ad0fed7956da6c0c
sultanbayev <sultanbayev@gmail.com>
2021-08-08 02:08:08 +0600

    update and add POST url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,2 @@
-export const URL = 'https://norma.nomoreparties.space/api/ingredients';
+export const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
+export const ORDERS_URL = 'https://norma.nomoreparties.space/api/orders/';
