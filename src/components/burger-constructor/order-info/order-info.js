7ab8edfbe4f5cd5189171c0793ed0059bef735bd
sultanbayev <sultanbayev@gmail.com>
2021-08-08 02:10:31 +0600

    order-info update

--- src/components/burger-constructor/order-info/order-info.js
+++ src/components/burger-constructor/order-info/order-info.js
@@ -1,15 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo, useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import styles from './order-info.module.css';
 import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import OrderDetails from '../../modal/order-details/order-details';
-import { ModalContext } from "../../app/context";
+import { ModalContext } from "../../app/app";
+import { ORDERS_URL } from '../../../constants/constants';
+import { BurgerConstructorContext } from "../burger-constructor";
 
 function OrderInfo({ total }) {
-    const {onModalOpen} = useContext(ModalContext);
+    const { onModalOpen } = useContext(ModalContext);
+    const [orderNumber, setOrderNumber] = useState(null);
+
+    const { componentsState } = useContext(BurgerConstructorContext);
+
+    const data = useMemo(() => {
+        if (componentsState.bun && componentsState.staffings.length !== 0) {
+            const staffingIds = componentsState.staffings.map(c => c._id)
+            return { ingredients: [componentsState.bun._id, componentsState.bun._id, ...staffingIds]}
+        }
+        return null;
+    }, [componentsState])
+
+    useEffect(() => {
+        if (!data) return;
+        try {
+            fetch(ORDERS_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                  },
+                body: JSON.stringify(data)
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    }
+                    return Promise.reject(response);
+                })
+                .then(data => {
+                    if (data.success) {
+                        setOrderNumber(data.order.number)
+                    }
+                })
+        } catch (error) {
+            console.log("Что-то пошло не так c заказом.", error);
+        }
+    }, [data])
 
     const onClick = () => {
-        onModalOpen(<OrderDetails orderNumber={'034536'} />);
+        onModalOpen(<OrderDetails orderNumber={orderNumber} />);
     }
 
     return (
