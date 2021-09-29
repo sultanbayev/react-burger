2a37c846610c5d0cb76c6628451f01f2165f06ae
sultanbayev <sultanbayev@gmail.com>
2021-09-29 17:51:31 +0600

    fix private route

--- src/components/private-route/private-route.js
+++ src/components/private-route/private-route.js
@@ -1,18 +1,22 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function PrivateRoute({ children, ...rest }) {
 
-    const { user } = useSelector(store => store.user);
+    const { isAuthorised } = useSelector(store => store.user);
 
     return (
-        <Route {...rest} render={props => (
-            (user)
-                ? children
-                : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        <Route {...rest} render={({ location }) => (
+            isAuthorised ? ( children )
+                : ( <Redirect to={{ pathname: '/login', state: { from: location } }} /> )
         )} />
     )
 }
 
 export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+    rest: PropTypes.object
+}
