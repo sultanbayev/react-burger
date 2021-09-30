8559f873129ff137605eaa517e7b47eb37676586
sultanbayev <sultanbayev@gmail.com>
2021-10-01 04:51:35 +0600

    refactor tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
+import { store } from './services/store';
 // import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
