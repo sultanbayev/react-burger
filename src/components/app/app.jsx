e0fa73b8a89a22ae0062c23e0e93f0aec7e9992a
sultanbayev <sultanbayev@gmail.com>
2021-10-18 06:13:46 +0600

    feature: update app routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,9 @@ import {
     IngredientPage,
     NotFoundPage,
     ProfileOrdersPage,
-    FeedPage } from '../../pages';
+    FeedPage,
+    FeedOrderPage,
+    ProfileOrderPage } from '../../pages';
 import BaseLayout from '../layouts/base-layout';
 import {
   BrowserRouter as Router,
@@ -18,27 +20,41 @@ import {
   useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import ProtectedRoute from '../protected-route/protected-route';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUserData } from '../../services/actions/user';
 import { fetchIngredients } from '../../services/actions/burger-ingredients';
 import IngredientDetails from '../modal/ingredient-details/ingredient-details';
 import Modal from '../modal/modal';
+import { USER_WS_CONNECTION_START, WS_CONNECTION_START } from '../../services/actions/wsActions';
+import FeedOrderInfo from '../feed-order-info/feed-order-info';
+import ProfileForm from '../profile-form/profile-form';
  
 export default function App() {
 
   const dispatch = useDispatch();
+  const { isAuthorised } = useSelector(store => store.user);
+  const refreshToken = localStorage.getItem('refreshToken');
 
   useEffect(() => {
-      const token = localStorage.getItem('refreshToken');
-      token && dispatch(getUserData());
+      if (refreshToken) {
+        dispatch(getUserData());
+      }
       dispatch(fetchIngredients());
       //eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    if (isAuthorised) {
+      dispatch({ type: WS_CONNECTION_START });
+      dispatch({ type: USER_WS_CONNECTION_START});
+    }
+    //eslint-disable-next-line
+  }, [isAuthorised]);
+
   return (
-        <Router>
-            <ModalSwitch />
-        </Router>
+    <Router>
+        <ModalSwitch />
+    </Router>
   );
 }
 
@@ -47,30 +63,63 @@ function ModalSwitch() {
   let history = useHistory();
   const location = useLocation();
 
-  const background = (history.action === 'PUSH') ? (location.state && location.state.background) : undefined;
+  const background = history.action === 'PUSH' && location.state && location.state.background;
   const ingredient = location.state && location.state.ingredient;
+  const order = location.state && location.state.order;
+
+  const onClose = () => {
+    history.goBack();
+  };
 
   return (
     <BaseLayout>
       <Switch location={ background || location } >
         <Route path="/" exact><HomePage /></Route>
-        <ProtectedRoute path="/profile" exact><ProfilePage /></ProtectedRoute>
-        <ProtectedRoute path="/profile/orders" exact><ProfileOrdersPage /></ProtectedRoute>
+        <Route path="/feed" exact><FeedPage /></Route>
+        <Route path="/feed/:id" exact><FeedOrderPage /></Route>
+        <ProtectedRoute path="/profile" exact>
+          <ProfilePage><ProfileForm /></ProfilePage>
+        </ProtectedRoute>
+        <ProtectedRoute path="/profile/orders" exact>
+          <ProfilePage><ProfileOrdersPage /></ProfilePage>
+        </ProtectedRoute>
+        <ProtectedRoute path="/profile/orders/:id" exact><ProfileOrderPage /></ProtectedRoute>
         <Route path="/login" exact><LoginPage /></Route>
         <Route path="/register" exact><RegisterPage /></Route>
         <Route path="/forgot-password" exact><ForgotPasswordPage /></Route>
         <Route path="/reset-password" exact><ResetPasswordPage /></Route>
         <Route path="/ingredient/:id" exact><IngredientPage /></Route>
-        <Route path="/feed" exact><FeedPage /></Route>
         <Route><NotFoundPage path="*" /></Route>
       </Switch>
 
-      {background && ingredient && <Route path="/ingredient/:id" render={() => {
-          const onClose = () => {
-            history.goBack();
-          };
-          return <Modal onClose={onClose}><IngredientDetails ingredient={ingredient} /></Modal>;
-        }} />}
+      { (background && ingredient) && (
+        <Route path="/ingredient/:id" render={() => {
+            return (
+              <Modal onClose={onClose}>
+                <IngredientDetails ingredient={ingredient} />
+              </Modal>
+            );
+          }} /> 
+      )}
+      { (background && order) && (
+        <Route path="/feed/:id" render={() => {
+            return (
+              <Modal onClose={onClose}>
+                <FeedOrderInfo order={order} />
+              </Modal>
+            );
+          }} /> 
+      )}
+      { (background && order) && (
+        <Route path="/profile/orders/:id" render={() => {
+            return (
+              <Modal onClose={onClose}>
+                <FeedOrderInfo order={order} />
+              </Modal>
+            );
+          }} /> 
+      )}
+      
     </BaseLayout>
   );
 }
