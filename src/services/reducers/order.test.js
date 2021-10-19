c5dde5b953341007b2c75dfec7666bcf275f3ca0
sultanbayev <sultanbayev@gmail.com>
2021-10-20 03:03:03 +0600

    test: fix orderReducer test

--- src/services/reducers/order.test.js
+++ src/services/reducers/order.test.js
@@ -18,28 +18,74 @@ describe('order reducer', () => {
     });
 
     it('should set orderRequest to TRUE', () => {
-        expect(orderReducer(initialState, { type: GET_ORDER_REQUEST })).toEqual({
-            ...initialState,
-            orderRequest: true,
-        });
+        expect(orderReducer(initialState, {
+                type: GET_ORDER_REQUEST
+            })).toEqual({
+                ...initialState,
+                orderRequest: true,
+            });
+
+        expect(orderReducer({
+                name: 'Black Hole Singularity острый бургер',
+                number: 4235,
+                orderRequest: false,
+                orderFailed: false,
+            }, { type: GET_ORDER_REQUEST })).toEqual({
+                name: 'Black Hole Singularity острый бургер',
+                number: 4235,
+                orderFailed: false,
+                orderRequest: true,
+            });
     });
 
     it('should set orderRequest to FALSE and orderFailed to TRUE', () => {
-        expect(orderReducer(initialState, { type: GET_ORDER_FAILED })).toEqual({
-            ...initialState,
-            orderRequest: false,
-            orderFailed: true,
-        });
+        expect(orderReducer(initialState, {
+                type: GET_ORDER_FAILED
+            })).toEqual({
+                ...initialState,
+                orderRequest: false,
+                orderFailed: true,
+            });
+
+        expect(orderReducer({
+                name: 'Black Hole Singularity острый бургер',
+                number: 4235,
+                orderRequest: false,
+                orderFailed: false,
+            }, {
+                type: GET_ORDER_FAILED
+            })).toEqual({
+                name: 'Black Hole Singularity острый бургер',
+                number: 4235,
+                orderRequest: false,
+                orderFailed: true,
+            });
     });
 
     it('should set order name and order number', () => {
         expect(orderReducer(initialState, {
                 type: GET_ORDER_SUCCESS,
                 name: 'Black Hole Singularity острый бургер',
-                number: '4235' })
-            ).toEqual({
+                number: 4235
+            })).toEqual({
                 name: 'Black Hole Singularity острый бургер',
-                number: '4235',
+                number: 4235,
+                orderRequest: false,
+                orderFailed: false,
+            });
+
+        expect(orderReducer({
+                name: 'Black Hole Singularity острый бургер',
+                number: 4235,
+                orderRequest: true,
+                orderFailed: false,
+            }, {
+                type: GET_ORDER_SUCCESS,
+                name: 'Interstellar бургер',
+                number: 4666
+            })).toEqual({
+                name: 'Interstellar бургер',
+                number: 4666,
                 orderRequest: false,
                 orderFailed: false,
             });
