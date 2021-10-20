5376d50fbdba6bc9e5f8e0f10cdd080634ddf801
sultanbayev <sultanbayev@gmail.com>
2021-10-20 06:36:18 +0600

    test: create drag-n-drop functional test

---
+++ cypress/integration/drag-n-drop.spec.js
@@ -0,0 +1,11 @@
+describe('Ensuring that drag-n-drop functionality works properly on Home page', function() {
+    before(function() {
+        cy.visit('http://localhost:3000');
+    });
+
+    
+
+
+  
+  
+})
