19b08efeedf7f1e7bce9d85d47e48dd549f6e3df
sultanbayev <sultanbayev@gmail.com>
2021-10-23 21:39:39 +0600

    update BaseLayout component to typescript

--- src/components/layouts/base-layout.tsx
+++ src/components/layouts/base-layout.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppHeader from '../app-header/app-header';
 import Main from '../main/main';
-import PropTypes from 'prop-types';
 
-function BaseLayout({ children }) {
+interface IBaseLayoutProps {
+    children: React.ReactNode;
+}
+
+const BaseLayout: React.FC<IBaseLayoutProps> = ({ children }) => {
     return (
         <>
             <AppHeader />
@@ -12,10 +15,6 @@ function BaseLayout({ children }) {
             </Main>
         </>
     )
-}
-
-BaseLayout.propTypes = {
-    children: PropTypes.node.isRequired
 }
 
 export default React.memo(BaseLayout);
