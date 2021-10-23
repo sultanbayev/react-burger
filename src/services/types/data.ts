178ebf2c75005e90b2e49ff37a9422c6004dbdfa
sultanbayev <sultanbayev@gmail.com>
2021-10-23 21:40:34 +0600

    add TOrder type

--- src/services/types/data.ts
+++ src/services/types/data.ts
@@ -21,3 +21,12 @@ export type TIngredientWithUuid = TIngredient & {
     uuid: string;
 }
 
+export type TOrder = {
+    _id: string;
+    ingredients: string[];
+    status: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    number: number;
+}
