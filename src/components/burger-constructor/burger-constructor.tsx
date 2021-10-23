f28cb05dedc8fe12e5890c646c0623300883fe13
sultanbayev <sultanbayev@gmail.com>
2021-10-23 21:24:42 +0600

    refactor BurgerConstructor component

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -2,24 +2,34 @@ import styles from './styles.module.css';
 import ComponentList from './component-list/component-list';
 import OrderInfo from './order-info/order-info';
 import Modal from '../modal/modal';
-import { useDispatch, useSelector } from '../../services/hooks';
-import { CLOSE_MODAL } from '../../services/actions/modal';
+import OrderDetails from '../modal/order-details/order-details';
+import { useState, useCallback, useEffect } from 'react';
+import { useSelector } from '../../services/hooks';
 
 function BurgerConstructor() {
 
-    const { isOpen, content } = useSelector(state => state.modal);
-    const dispatch = useDispatch();
+    const [open, setOpen] = useState(false)
 
-    const onModalClose = () => {
-        dispatch({ type: CLOSE_MODAL });
-    }
+    const onClose = useCallback(() => {
+        setOpen(false);
+    }, []);
+
+    const { number } = useSelector(state => state.order);
+
+    useEffect(() => {
+        if ( number ) {
+            setOpen(true);
+        }
+    }, [number])
 
     return (
-        <section className={styles.container}>
-            <ComponentList />
-            <OrderInfo />
-            { isOpen && <Modal onClose={onModalClose}>{content}</Modal> }
-        </section>
+        <>
+            <section className={styles.container}>
+                <ComponentList />
+                <OrderInfo />
+            </section>
+            { (open && number) && <Modal onClose={onClose}><OrderDetails orderNumber={number} /></Modal> }
+        </>
     );
 }
 
